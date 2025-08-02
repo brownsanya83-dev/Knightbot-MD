3b82b4f0b8390ec6349a276bab6cbe27a963701f
brownsanya83-dev <brownsanya83@gmail.com>
2025-08-02 05:12:48 -0500

    Update settings.js
    There's no other changes

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Talia Bot',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "Talia Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '18765653776', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
